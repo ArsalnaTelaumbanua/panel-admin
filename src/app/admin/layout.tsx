a4c2cdd524e17a6d087840767181655bd2760c2e
ArsalnaTelaumbanua <arsalnatelaumbanua@gmail.com>
2024-12-17 23:26:24 +0700

    commit4

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,18 @@
+"use client";
+import { useAppSelector } from "@/redux/hooks";
+import { useSession } from "next-auth/react";
 import React from "react";
+import Login from "../components/admin-apanel/Login";
 
 const Layout = () => {
+
+    const isLoading = useAppSelector((store) => store.LoadingReducer);
+    const {data: session} = useSession();
+
+    if(!session?.user){
+        return <Login />;
+    }
+
     return <div>layout</div>;
 };
 
